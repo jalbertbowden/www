8e977786db9b38745443c9b7ddc4b8f0d8c6e14b
albert <jalbertbowden@gmail.com>
2019-07-19 11:16:16 -0400

    Add files via upload

---
+++ python/cors_http_server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# via: https://stackoverflow.com/a/21957017/620065
+
+# Run in the directory you want to serve files from:
+# python cors_http_server.py 3000
+
+try:
+    # Python 3
+    from http.server import HTTPServer, SimpleHTTPRequestHandler, test as test_orig
+    import sys
+    def test (*args):
+        test_orig(*args, port=int(sys.argv[1]) if len(sys.argv) > 1 else 3000)
+except ImportError: # Python 2
+    from BaseHTTPServer import HTTPServer, test
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer)
